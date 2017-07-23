69398eacf3db53450d0fee4a7210d730588a1e4f
dat <dat@coachingcloud.com>
2017-07-23 22:12:45 +1200

    refactor semicolon

--- gather.sh
+++ gather.sh
@@ -6,4 +6,4 @@ cp ~/.utilities ./zsh/
 cp ~/.config/nvim/init.vim ./nvim/
 cp ~/.vimrc ./vim/
 
-cp ~/.shortcuts/* ./shortcuts/
+cp ~/.semicolon/* ./semicolon/
--- shortcuts/copyq_s1.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-copyq tab clipboard copy "`copyq tab notes read 0 | rev | cut -c 1- | rev`"
