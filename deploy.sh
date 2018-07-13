ccfc42235cf82db1bba8a2224b7bca2ec844a2ef
duydatyds <duydatyds@gmail.com>
2018-07-13 23:46:38 +1200

    update semicolon

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cp ./zsh/.zshrc ~/
+cp ./zsh/.aliases ~/
+cp ./zsh/.utilities ~/
+
+cp ./nvim/init.vim ~/.config/nvim/
+cp ./vim/.vimrc ~/
+
+cp ./tmux/.tmux.conf ~/
+
+cp ./semicolon/main.sh ~/.semicolon/
+cp ./semicolon/package.json ~/.semicolon 
+cp -r ./semicolon/utils ~/.semicolon/
+
