d2743c0541d90e72371bec84458df21e1dc9794f
Dat Pham <duydatyds@gmail.com>
2019-05-13 07:10:24 +1200

    add smart detect delimiter for csv-input

--- semicolon/utils/adapters/csv-input.js
+++ semicolon/utils/adapters/csv-input.js
@@ -1,11 +1,40 @@
+const os = require('os');
 const csv2json = require('../libs/csv2json');
+
+function detectDelimiter(input, _) {
+  const firstLine = _.chain(input)
+    .split(os.EOL)
+    .first()
+    .value()
+
+  return _.chain([';', ',', "\t"])
+    .map((delimiter) => {
+      return {
+        delimiter,
+        count: _.split(firstLine, delimiter).length,
+      }
+    })
+    .maxBy('count')
+    .get('delimiter')
+    .value()
+}
 
 function handle(chain, _, rawConfig) {
   return chain
     .thru(input => {
-      const config = JSON.parse(rawConfig || '{}');
+      const config = eval(`(${rawConfig})`);
       const hasHeader = _.get(config, 'header', true);
-      const delimiter = _.get(config, 'delimiter', ',');
+      let delimiter = _.get(config, 'delimiter', null);
+
+      switch (delimiter) {
+        case '<tab>': delimiter = "\t"; break;
+        case '<cr>': delimiter = "\r\n"; break;
+        case '<eol>': delimiter = os.EOL; break;
+      }
+
+      if (!delimiter) {
+        delimiter = detectDelimiter(input, _);
+      }
 
       const csv = csv2json(_.trim(input), delimiter);
 
