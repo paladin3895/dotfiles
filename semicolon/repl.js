ec0a46cd8792f5cb7765732abf772fe00de671d0
Dat Pham <duydatyds@gmail.com>
2019-09-15 04:28:46 +1200

    add hyper

---
+++ semicolon/repl.js
@@ -0,0 +1,23 @@
+/**
+ * Spawns an embedded neovim instance and returns Neovim API
+ */
+
+const cp = require('child_process');
+const attach = require('neovim').attach;
+// const inspect = require('util').inspect;
+
+module.exports = (async function() {
+  let proc;
+  let socket;
+
+  if (process.env.NVIM_LISTEN_ADDRESS) {
+    socket = process.env.NVIM_LISTEN_ADDRESS;
+  } else {
+    proc = cp.spawn('nvim', ['-u', 'NONE', '-N', '--embed'], {
+      cwd: __dirname,
+    });
+  }
+
+  const nvim = await attach({ proc, socket });
+  return nvim;
+})();
