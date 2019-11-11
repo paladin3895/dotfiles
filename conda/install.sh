14110196a0ebd3b5ed6b1d521e7f2a90b83e7bc7
Dat Pham <dat@72dpi.co.nz>
2019-11-12 12:20:13 +1300

    add conda init script

---
+++ conda/install.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install python3.7
+sudo apt install python3-pip
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+
+conda install ipython
+conda install jupyter
+conda install jupyterlab
+
+pip install wikipedia
+pip install googletrans
